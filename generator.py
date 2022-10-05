5ff9206da1736c5d159a028f21583d9de51be1d3
Perez-salem Tuwan <perezjoshua1811@gmail.com>
2022-10-05 12:04:50 +0100

    initialize 'Generator' class

--- generator.py
+++ generator.py
@@ -1,4 +1,9 @@
 class Generator():
     
     def __init__(self) -> None:
+        self.length = 10
+        toogleSym = False
+        toogleLow = True
+        toogleUpp = True
+        toogleNum = True
         pass
