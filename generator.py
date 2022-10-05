9ec5394f3b212c3d2c6b5c3148fe81cfaa58e79c
Perez-salem Tuwan <perezjoshua1811@gmail.com>
2022-10-05 12:11:25 +0100

    add setters and getters to 'Generator' class

--- generator.py
+++ generator.py
@@ -7,3 +7,34 @@ class Generator():
         self._toogleUpp = True
         self._toogleNum = True
         pass
+    
+    def get_length(self,):
+        return self.length
+
+    def is_symbol(self,):
+        return self._toogleSym
+
+    def is_lowercase(self,):
+        return self._toogleLow
+
+    def is_uppercase(self,):
+        return self._toogleUpp
+
+    def is_num(self,):
+        return self._toogleNum
+
+
+    def set_num(self, value):
+        self._toogleNum = value
+
+    def set_lowercase(self, value):
+        self._toogleLow = value
+
+    def set_uppercase(self, value):
+        self._toogleUpp = value
+
+    def set_sym(self, value):
+        self._toogleSym = value
+
+    def set_length(self, newLength):
+        self._length = newLength
