ee29f98dde5bb1b398345d2fe45c1ceff8c277b6
Perez-salem Tuwan <perezjoshua1811@gmail.com>
2022-10-15 17:09:34 +0100

    update 'store-record()' and fix typos

--- recordmanager.py
+++ recordmanager.py
@@ -1,14 +1,23 @@
 class Record_mgr:
     
     
-    def __init__():
-        pass
+    def __init__(self,):
+        self.currentFile = ''
 
 
-    def store_record():
-        pass
-    
+    def store_record(self, partial_path, fileName, record):
+        
+        # stop if record is invalid or path is invalid
+        if(record['type'] != 'password'):
+            return "failure"
+        
+        # write to path
+        self.currentFile = partial_path + fileName
+        recordFile = open(r"" + self.currentFile , "w")
+        for i in record.keys():
+            recordFile.write(record[i] + ' /' + i)
 
-    def get_record():
+
+    def get_record(self,):
         pass
     
